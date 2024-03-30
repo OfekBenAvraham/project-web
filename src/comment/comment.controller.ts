549fac4b0f256096431de372102f1352b6082036
ofek <benavrahamofek@gmail.com>
2024-03-30 13:35:47 +0300

    fix auth

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Get, Param, Post, Request, UseGuards} from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
-import { AuthGuard } from '@nestjs/passport';
 import { JwtService } from '@nestjs/jwt';
 
 @Controller('comment')
@@ -9,11 +8,13 @@ export class CommentController {
   constructor(private readonly commentService: CommentService, private jwtService: JwtService) {}
 
   @Post()
-  @UseGuards(AuthGuard('jwt'))
   create(@Body() createCommentDto: CreateCommentDto, @Request() req) {
     const token = req.headers.authorization.split(' ')[1];
     const decoded = this.jwtService.decode(token);
-    const userId = decoded.sub; // user's ID
+    let userId = null;
+    if(decoded) {
+      userId = decoded.sub; // user's ID
+    }
     return this.commentService.create(createCommentDto, userId);
   }
 
